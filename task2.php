15c6c0617ba37679482cddb8e6e3853ea9d639dc
glebasix <glebasix@gmail.com>
2022-07-08 19:53:45 +0300

    add debug file and new tasks

--- task2.php
+++ task2.php
@@ -15,33 +15,101 @@ print_r($array);
 //////////////
 // task 2
 
-$arrayOne = ['a', 'b', 'c', 'd', 'e'];
-$arrayTwo = ['a', 'd', 'c', 'b', 'e'];
-foreach ($arrayOne as $key => $value)
-    if ($value == $arrayTwo[$key]) {
-        $arrayFinish[$key] = $value;
-    }
-print_r($arrayFinish);
+$array = ['President'];
+$arrayCompare = ['side'];
+$arrayAfterCompare = substr($array[0], strpos($array[0], $arrayCompare[0]), strlen($arrayCompare[0]));
+print_r($arrayAfterCompare);
 
 //////////////////
 // task 3
 
-$arrayDad = [2, 1, 1, 2, 3, 3, 2, 2, 2, 1];
-$n = 0;
-$m = 0;
-$c = 0;
-foreach ($arrayDad as $key => $value) {
-    if ($value == $arrayDad[$key++]) {
-        $n++;
-    } elseif ($value !== $arrayDad[$key++]) {
-        if ($m < $n) {
-            $m = $n;
-            $c = $key - $n;
-            $n = 0;
+$array = [2,1,1,2,3,3,2,2,2,1];
+$arraySequence = [];
+foreach ($array as $key => $value) {
+    $arrayCount[] = $value;
+    if ($value === $array[$key + 1]) {
+        continue;
+    } else {
+        if (count($arraySequence) < count($arrayCount)) {
+            unset($arraySequence);
+            $arraySequence = $arrayCount;
+            unset($arrayCount);
+        } else {
+            unset($arrayCount);
         }
     }
 }
-echo $n;
-echo $m;
-echo $c;
-print_r(array_slice($arrayDad,$c,$m++));
+print_r($arraySequence);
+
+////////////////////////////////
+// task 4
+
+$array = [3,2,3,4,2,2,4];
+$arraySequence = [];
+foreach ($array as $key => $value) {
+    $arrayCount[] = $value;
+    if ($value === $array[$key + 1] - 1) {
+        continue;
+    } else {
+        if (count($arraySequence) < count($arrayCount)) {
+            unset($arraySequence);
+            $arraySequence = $arrayCount;
+            unset($arrayCount);
+        } else {
+            unset($arrayCount);
+        }
+    }
+}
+print_r($arraySequence);
+
+////////////////////////////////
+// task 5
+
+$array = [4,1,1,4,2,3,4,4,1,2,4,9,3];
+$count = count($array);
+for ($i = 0; $i < $count-1; $i++) {
+    for ($j = 0; $j < $count - 1 - $i; $j++) {
+        if ($array[$j] > $array[$j+1]) {
+            $old = $array[$j];
+            $array[$j] = $array[$j+1];
+            $array[$j+1] = $old;
+        }
+    }
+}
+print_r($array);
+
+//////////////////////////
+// task 6
+
+$array = [4,1,1,4,2,3,4,4,1,2,4,9,3];
+$count = count($array);
+for ($i = 0; $i < $count-1; $i++) {
+    for ($j = 0; $j < $count - 1 - $i; $j++) {
+        if ($array[$j] > $array[$j+1]) {
+            $old = $array[$j];
+            $array[$j] = $array[$j+1];
+            $array[$j+1] = $old;
+        }
+    }
+}
+$arrayFrequent = [];
+foreach ($array as $key => $value) {
+    $arrayCount[] = $value;
+    if ($value === $array[$key + 1]) {
+        continue;
+    } else {
+        if (count($arrayFrequent) < count($arrayCount)) {
+            unset($arrayFrequent);
+            $arrayFrequent = $arrayCount;
+            unset($arrayCount);
+        } else {
+            unset($arrayCount);
+        }
+    }
+}
+print_r($arrayFrequent);
+
+////////////////////////////////////
+// task 7
+
+
