aa933349a00e4fc05f70ce8da4c8fe903626752a
glebasix <glebasix@gmail.com>
2022-07-11 18:50:12 +0300

    add debug file complete all array tasks

--- task2.php
+++ task2.php
@@ -26,16 +26,20 @@ print_r($arrayAfterCompare);
 $array = [2, 1, 1, 2, 3, 3, 2, 2, 2, 1];
 $arraySequence = [];
 foreach ($array as $key => $value) {
-    $arrayCount[] = $value;
-    if ($value === $array[$key + 1]) {
-        continue;
+    if (!isset($array[$key + 1])) {
+        break;
     } else {
+        $arrayCount[] = $value;
+        if ($value === $array[$key + 1]) {
+            continue;
+        } else {
         if (count($arraySequence) < count($arrayCount)) {
             unset($arraySequence);
             $arraySequence = $arrayCount;
             unset($arrayCount);
         } else {
             unset($arrayCount);
+        }
         }
     }
 }
@@ -47,16 +51,21 @@ print_r($arraySequence);
 $array = [3, 2, 3, 4, 2, 2, 4];
 $arraySequence = [];
 foreach ($array as $key => $value) {
-    $arrayCount[] = $value;
-    if ($value === $array[$key + 1] - 1) {
-        continue;
+    if (!isset($array[$key + 1])) {
+        break;
     } else {
-        if (count($arraySequence) < count($arrayCount)) {
-            unset($arraySequence);
-            $arraySequence = $arrayCount;
-            unset($arrayCount);
+        $arrayCount[] = $value;
+
+        if ($value === $array[$key + 1] - 1) {
+            continue;
         } else {
-            unset($arrayCount);
+            if (count($arraySequence) < count($arrayCount)) {
+                unset($arraySequence);
+                $arraySequence = $arrayCount;
+                unset($arrayCount);
+            } else {
+                unset($arrayCount);
+            }
         }
     }
 }
@@ -67,12 +76,12 @@ print_r($arraySequence);
 
 $array = [4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3];
 $count = count($array);
-for ($i = 0; $i < $count-1; $i++) {
+for ($i = 0; $i < $count - 1; $i++) {
     for ($j = 0; $j < $count - 1 - $i; $j++) {
-        if ($array[$j] > $array[$j+1]) {
+        if ($array[$j] > $array[$j + 1]) {
             $old = $array[$j];
-            $array[$j] = $array[$j+1];
-            $array[$j+1] = $old;
+            $array[$j] = $array[$j + 1];
+            $array[$j + 1] = $old;
         }
     }
 }
@@ -82,55 +91,37 @@ print_r($array);
 // task 6
 
 $array = [4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3];
-$count = count($array);
-for ($i = 0; $i < $count-1; $i++) {
-    for ($j = 0; $j < $count - 1 - $i; $j++) {
-        if ($array[$j] > $array[$j+1]) {
-            $old = $array[$j];
-            $array[$j] = $array[$j+1];
-            $array[$j+1] = $old;
+$arrayBig = [];
+for ($i = 0; $i < count($array); $i++) {
+    for ($j = 0; $j < count($array); $j++) {
+        if ($array[$j] === $array[$i]) {
+            $arrayCount[] += $array[$j];
         }
     }
-}
-$arrayFrequent = [];
-foreach ($array as $key => $value) {
-    $arrayCount[] = $value;
-    if ($value === $array[$key + 1]) {
-        continue;
+    if (count($arrayBig) < count($arrayCount)) {
+        unset ($arrayBig);
+        $arrayBig = $arrayCount;
+        unset ($arrayCount);
     } else {
-        if (count($arrayFrequent) < count($arrayCount)) {
-            unset($arrayFrequent);
-            $arrayFrequent = $arrayCount;
-            unset($arrayCount);
-        } else {
-            unset($arrayCount);
-        }
+        unset ($arrayCount);
     }
 }
-print_r($arrayFrequent);
+print_r($arrayBig);
 
 ////////////////////////////////////
 // task 7
 
 $array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-$item = 7;
-function binarySearch($array, $item, $start = 0, $end = null)
-{
-    if ($end === null) {
-        $end = count($array) - 1;
+$item = 9;
+$low = 0;
+$high = count($array) - 1;
+$halfIndex = (int) (($low + $high) / 2);
+While ($array[$halfIndex] !== $item) {
+    if ($array[$halfIndex] < $item) {
+        $low = $halfIndex +1;
+    } else {
+        $high = $halfIndex - 1;
     }
-    if ($start > $end) {
-        echo "Item not found";
-    }
-    $halfIndex = (int) (($start + $end) / 2);
-    if ($halfIndex !== $item) {
-        if ($halfIndex < $item) {
-            $start = $halfIndex + 1;
-        } else {
-            $end = $halfIndex - 1;
-        }
-        return binarySearch($array, $item, $start, $end);
-    }
-    return $halfIndex;
+    $halfIndex = (int) (($low + $high) / 2);
 }
-echo binarySearch($array, $item);
+echo $halfIndex;
