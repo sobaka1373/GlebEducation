9dfa4f41a1b7cb9b201a44e62126b8f0978b720c
glebasix <glebasix@gmail.com>
2022-07-19 14:49:25 +0300

    add two new files

---
+++ task 6.php
@@ -0,0 +1,6 @@
+<form action="task%206%20additional.php" method="get">
+    <p>Создайте квадратную матрицу</p>
+    <p>Введите размерность от 1 до 10:</p>
+    <input type="text" name="number">
+    <input type="submit" value="Создать">
+</form>
